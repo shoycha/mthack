e519f18d09e23e85a64dcb24640ddefeadb1e9fb
shoycha <shoira.shamsieva@gmail.com>
2017-02-04 15:36:33 -0600

    loading backup

---
+++ insert.php
@@ -0,0 +1,46 @@
+
+<?php
+
+ $con = mysqli_connect('127.0.0.1','ss6k','mthack2017');
+
+if(!$con)
+{
+	echo 'Not Connected To Server';
+}
+
+
+if(!mysqli_select_db($con,'ss6k'))
+{
+	echo 'Database Not Selected';
+}
+
+
+$FName = $_POST['firstname'];
+$LName = $_POST['lastname'];
+$UID = $_POST['userid'];
+$Course = $_POST['course'];
+$Section = $_POST['section'];
+
+//var_dump($_POST);
+
+
+ $sql ="INSERT INTO Student (LastName,FirstName,UserID,CourseName,Section) 
+	VALUES ('$FName','$LName','$UID','$Course','$Section')";
+
+
+
+if(!mysqli_query($con,$sql))
+{
+	echo '<h2>Not Inserted</h2><br>';
+	echo '<h2>', $UID, ' is already signed in for ' , $Course, '</h2>';
+}
+
+else
+{
+	echo 'Inserted';
+}
+
+header("refresh:2; url=index.html");
+
+
+?>
