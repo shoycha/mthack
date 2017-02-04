913fd73c14411606805a9262cc67d5300306bcce
shoycha <shoira.shamsieva@gmail.com>
2017-02-04 13:40:48 -0600

    fixed bugs

--- insert.php
+++ insert.php
@@ -1,31 +1,38 @@
+
+
 <?php
-$con = mysqli_connect('127.0.0.1','root','');
+//echo 'insert php'
+ $con = mysqli_connect('127.0.0.1','ss6k','mthack2017');
 
 if(!$con)
 {
 	echo 'Not Connected To Server';
 }
 
+
 if(!mysqli_select_db($con,'ss6k'))
 {
 	echo 'Database Not Selected';
 }
 
-$FName = $_POST('firstname');
-$LName = $_POST('lastname');
-$UID = $_POST('userid');
-$Course = $_POST('course');
-$Section = $_POST('section');
+
+$FName = $_POST['firstname'];
+$LName = $_POST['lastname'];
+$UID = $_POST['userid'];
+$Course = $_POST['course'];
+$Section = $_POST['section'];
+
+var_dump($_POST);
 
 
-
-
-$sql = "INSERT INTO Student (LastName,FirstName,UserID,CourseName,Section,TimeStamp,Lat,Lng) 
+ $sql ="INSERT INTO Student (LastName,FirstName,UserID,CourseName,Section) 
 	VALUES ('$FName','$LName','$UID','$Course','$Section')";
-	
+
+
+
 if(!mysqli_query($con,$sql))
 {
-	echo 'Not Inserted'
+	echo 'Not Inserted';
 }
 
 else
@@ -34,8 +41,6 @@ else
 }
 
 header("refresh:2; url=index.html");
-
-
 
 
 ?>
